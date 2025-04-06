b097d80b8869121ce838ccec0ca8300ee8e26321
Greskad74 <zayaman74@gmail.com>
2025-04-06 14:53:44 -0600

    Prototipo

--- src/librerias/gpio.c
+++ src/librerias/gpio.c
@@ -6,7 +6,30 @@ void init_gpio(void){
 	RCC->APB2ENR |= 1 << 2; //enable clk
 	
 
+	//pa1
+	GPIOA -> CRL &=  ~(1<<4)|(1<<5);
+	GPIOA -> CRL |= (1<<7);
+	GPIA -> CRL |= (0 <<6);
+	
+
+	//PA2
+	
+	
+	GPIOA -> CRL &= ~(1<<9)(1<<8);
+	GPIOA -> CRL |=  (1<<11);
+	GPIOA -> CRL &= ~ (1<<10);
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/***********pwm***************/
+	
+	
 	//PA0 mode 2MHz.
 	GPIOA->CRL |= (1 << 1);
 	GPIOA->CRL &= ~(1 << 0);
@@ -15,9 +38,14 @@ void init_gpio(void){
 	//PA alternate function pushpull
 	GPIOA->CRL |= (1 << 3);
 	GPIOA->CRL &= ~(1 << 2);
-// Timer 3
+// Timer 3 PA6
 	GPIOA -> CRL |= (1 << 27);
 	GPIOA -> CRL &= ~(1 << 26);
 	GPIOA -> CRL |= (1 << 25); 
 	GPIOA -> CRL &=  ~( 1 << 24);
+
+
+
+
+
 }
